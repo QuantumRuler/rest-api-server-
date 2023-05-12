46c30980045d69aa675fd02523da1a296730c742
QuantumRuler <bvedant8@gmail.com>
2023-05-13 01:41:29 +0530

    RePushing all the code after final check

--- index.js
+++ index.js
@@ -2,16 +2,51 @@ const express = require('express')
 const app = express();
 
 app.get('/',(req,res) =>{
-   res.send('hello world');  
+   res.send('put /students for list all students');  
 });
+app.get('/students/', async (req,res) =>{
+    let p = await fetch("https://1qy9d2uuyg.execute-api.ap-northeast-1.amazonaws.com/default/Students-API").then((response) =>{
+        return response.json();
+    }).then(data => {
+        //console.log(data);
+        return data;
+    }).catch((err)  => {
+        console.log('rejected',err);
+    });
+    p.sort(function(a,b) {
+        if (a.name.toLowerCase() < b.name.toLowerCase()){
+            return -1;
+        }
+        if (b.name.toLowerCase() < a.name.toLowerCase()){
+            return 1;
+        }
+        return 0;
+    })
+    res.send(p)
+})
+app.get('/students/:college', async (req,res) =>{
+     let p = await fetch("https://1qy9d2uuyg.execute-api.ap-northeast-1.amazonaws.com/default/Students-API").then((response) =>{
+        return response.json();
+    }).then(data => {
+        return data;
+    }).catch((err)  => {
+        console.log('rejected',err);
+    });
+    function format(roll,name,college){
+        this.roll = roll;
+        this.name = name;
+        this.college = college;
+    }
 
-app.get('/api/courses',(rep,res) => {
-    res.send([1,2,3,4]);
-});
-
-app.get('/api/courses/:id',(req,res) =>{
-    res.send(`You sent id ${req.params.id}`)
-});
-
+    students = new Array();
+    p.forEach(item => {
+        if(item["college"] == req.params.college){
+            students.push(new format( item["roll no."],item["name"],item["college"]))
+        }           
+    });
+    res.send(students)
+  
+    
+})
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port} ....`));
